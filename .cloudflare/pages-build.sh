ac3317adbb92477483f91e8993565525e8b2f5e5
Raito <ninsho@lisa-rec.com>
2026-09-06 18:56:14 +0900

    Add Cloudflare Pages build script

---
+++ .cloudflare/pages-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+rm -rf _site
+mkdir -p _site/works _site/press
+
+cp index.html 404.html site.css site.js 404.css favicon.svg og-raito.png robots.txt sitemap.xml llms.txt .nojekyll _site/
+cp -R assets _site/
+cp works/index.html works/works.css works/works.js _site/works/
+cp press/index.html press/press.css _site/press/
